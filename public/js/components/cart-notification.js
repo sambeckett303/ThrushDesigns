9538dca9d49b8f8e5bb5db79d18988d45d841a82
Sam Beckett <mojobeckett@Sam-Becketts-MacBook-Pro.local>
2018-09-09 21:34:53 -0600

    update

--- public/js/components/cart-notification.js
+++ public/js/components/cart-notification.js
@@ -9,7 +9,7 @@ Vue.component('cart-notification',
 		<div v-if="show" id="notificationContainer">
 			<div id="successIcon"></div>
 			<div id="cartMessage">Successfully added item to your cart.</div>
-			<a href="/cart"><div style="margin-left: 30px;vertical-align: top;margin-top: 5px;" class="thrushButton">GO TO CART</div></a>
+			<a href="/cart"><div class="thrushButton">GO TO CART</div></a>
 		</div>
 		`
 });
