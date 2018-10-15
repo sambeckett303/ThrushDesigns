d30c89712c3bfb53a8551ea8805a77ba374e3177
Sam Beckett <mojobeckett@Sam-Becketts-MacBook-Pro.local>
2018-10-14 19:49:28 -0600

    fix

--- public/js/work.js
+++ public/js/work.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
 				var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 				if (w < 500)
 				{
-					return '275px';
+					return '350px';
 				}
 				else
 				{
